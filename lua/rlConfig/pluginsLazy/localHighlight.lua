23f9df37f6d2db460688afcba02b9557f3ee1407
Ryan Lloyd <ryan.lloyd@xplm.com>
2025-12-08 09:27:16 -0500

    Add Local highlight plugin

---
+++ lua/rlConfig/pluginsLazy/localHighlight.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		'tzachar/local-highlight.nvim',
+		config = function()
+			require('local-highlight').setup({
+				-- file_types = {}, -- If this is given only attach to this
+				-- OR attach to every filetype except:
+				disable_file_types = {},
+				hlgroup = 'LocalHighlight',
+				cw_hlgroup = nil,
+				-- Whether to display highlights in INSERT mode or not
+				insert_mode = false,
+				min_match_len = 1,
+				max_match_len = math.huge,
+				highlight_single_match = true,
+				animate = {
+					enabled = false,
+					easing = "linear",
+					duration = {
+						step = 10, -- ms per step
+						total = 100, -- maximum duration
+					},
+				},
+				debounce_timeout = 200,
+			})
+		end
+	},
+}
